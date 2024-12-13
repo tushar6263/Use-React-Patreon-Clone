858b799556e7c445388a9a7c3e842ad7f39ba8cf
tushar626346 <tushar626346@gmail.com>
2024-12-13 14:33:33 +0530

    Add Navbar and footer

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-   <div>
-    home page
-   </div>
+    <div className="flex justify-center gap-4 text-white items-center flex-col h-[40vh]">
+      <div className="font-bold text-3xl">Buy Me a Chai</div>
+      <p>
+        A crowdfunding platform for creators. Get funded by your fans and followers.Start now!
+      </p>
+      <div>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+      </div>
+    </div>
   );
 }
