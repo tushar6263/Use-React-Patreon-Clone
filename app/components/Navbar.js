058e86cf5a5b921fb7f2031b78587104290d5c76
tushar626346 <tushar626346@gmail.com>
2024-12-15 19:27:01 +0530

    Add the signproviders like github ,apple ,facebook etc

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,7 +4,18 @@ import { useSession,signIn,signOut } from 'next-auth/react'
 import Link from 'next/link'
 
 const Navbar = () => {
+    const {data : session} = useSession()
+    if (session) {
+        return (
+          <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        )
+    }
+    
     return (
+       
         <nav className='bg-black shadow-lg shadow-white text-white flex justify-between items-center px-4 h-16 '>
             <div className="logo font-bold text-lg flex justify-center items-center "> <img src="tea.gif" width={44} alt="" /> <span> GetMeaChai!</span></div>
             {/* <ul className= ' flex justify-between gap-4 cursor-pointer'>
@@ -17,10 +28,12 @@ const Navbar = () => {
 
             <div>
                 <Link href={"/login"}>
-            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button></Link>
+            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button></Link>
             </div>
         </nav>
+        
     )
+
 }
 
 export default Navbar
