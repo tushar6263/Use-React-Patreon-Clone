15069d22815e74a96b35d9be5104fff1627a15ec
tushar626346 <tushar626346@gmail.com>
2024-12-16 21:25:02 +0530

    ..

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,21 +1,21 @@
 "use client"
 import React from 'react'
-import { useSession,signIn,signOut } from 'next-auth/react'
+import { useSession, signIn, signOut } from 'next-auth/react'
 import Link from 'next/link'
 
 const Navbar = () => {
-    const {data : session} = useSession()
-    if (session) {
-        return (
-          <>
-            Signed in as {session.user.email} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </>
-        )
-    }
-    
+    const { data: session } = useSession()
+    // if (session) {
+    //     return (
+    //         <>
+    //             Signed in as {session.user.email} <br />
+    //             <button onClick={() => signOut()}>Sign out</button>
+    //         </>
+    //     )
+    // }
+
     return (
-       
+
         <nav className='bg-black shadow-lg shadow-white text-white flex justify-between items-center px-4 h-16 '>
             <div className="logo font-bold text-lg flex justify-center items-center "> <img src="tea.gif" width={44} alt="" /> <span> GetMeaChai!</span></div>
             {/* <ul className= ' flex justify-between gap-4 cursor-pointer'>
@@ -27,11 +27,15 @@ const Navbar = () => {
     </ul> */}
 
             <div>
-                <Link href={"/login"}>
-            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button></Link>
+                {session && <Link href={"/dashboard"}><button
+                    className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Dashboard</button></Link>}
+                {session && <button
+                    className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2' onClick={() => { signOut() }}>Logout</button>}
+                {!session &&<Link href={"/login"}>
+                    <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button></Link>}
             </div>
         </nav>
-        
+
     )
 
 }
